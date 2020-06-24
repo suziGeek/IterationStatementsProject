52502b6ad75267f506228ee8ddd036aed244e490
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-06-24 12:51:16 -0400

    still one error on for loop

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -11,33 +11,56 @@ namespace IterationStatements
 
             //Create a List called "numbers" - DONE
             var numbers = new List<int>();
-            
-            //Create a variable of type int and initialize the variable with a value of 0
+
+            //Create a variable of type int and initialize the variable with a value of 0 -DONE
+
+            int newNumbers = 0;
 
 
 
             // Create a do-while loop
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
+
+                do
+                {
+                   
+                    // Increment your variable by 1
+                    newNumbers++;
+
+                    // Then add your variable to "numbers"
+                    numbers.Add(newNumbers);
+                    //numbers.remove(newNumber);
+
+                    // While your variable is less than 100 - 
+                } while (newNumbers < 100);
+
+                
+                
             }
-            // While your variable is less than 100
+           
 
 
 
             // Create a while loop
             // While your variable is less than 200
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
+                while (newNumbers < 200) {
+                    // Increment your variable by 1
+                    newNumbers++;
+
+                    // Then add your variable to "numbers"
+                    numbers.Add(newNumbers);
+                }
             }
 
             Console.WriteLine("Increase:");
 
             // Create a foreach loop
+            foreach (newNumbers) { 
             // Write your variable to the console
 
-            Console.WriteLine("");
+            Console.WriteLine("${newNumbers}");
+            }
             Console.WriteLine("Decrease:");
 
             // Create a for loop
@@ -45,8 +68,10 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the length of "numbers"
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
+            for(var i= 199; i <= numbers.Length && i<= 0; i--)
             {
                 // Write to the console "numbers" at index i
+                Console.WriteLine("${numbers[i]}");
             }
         }
     }
